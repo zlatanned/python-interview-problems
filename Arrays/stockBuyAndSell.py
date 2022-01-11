d1c31e1b3b835ba6d62aeb2832a7297fa71d55ee
zlatanned <shahiakshay31@gmail.com>
2022-01-11 23:58:29 +0530

    solved buy and sell stock

---
+++ Arrays/stockBuyAndSell.py
@@ -0,0 +1,17 @@
+from typing import List
+"""
+    You can not sell before you buy
+"""
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) <1:
+            return 0
+        min_price, max_profit = prices[0], 0
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = price - min_price
+            max_profit = max(max_profit, profit)
+        return max_profit
+
+result = Solution()
+result.maxProfit([7, 1, 5, 3, 6, 4])
