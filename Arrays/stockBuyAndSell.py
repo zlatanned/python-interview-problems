54c9a2d65efb9c950c4f721e149eeadc5a1dcdda
zlatanned <shahiakshay31@gmail.com>
2022-01-12 00:13:25 +0530

    comment added for clarity

--- Arrays/stockBuyAndSell.py
+++ Arrays/stockBuyAndSell.py
@@ -9,7 +9,7 @@ class Solution:
         min_price, max_profit = prices[0], 0
         for price in prices:
             min_price = min(min_price, price)
-            profit = price - min_price
+            profit = price - min_price # profit if sold at curr elem.
             max_profit = max(max_profit, profit)
         return max_profit
 
